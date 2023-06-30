3e3d165b4a242b0ee916011190024a74a210bc34
ZEPHYR <democrat18906@gmail.com>
2023-06-30 22:40:58 +0500

    Create AccountsNet.py

---
+++ AccountsNet.py
@@ -0,0 +1,62 @@
+from telethon import TelegramClient, sync
+from telethon import events
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.functions.messages import SendMessageRequest
+from telethon.tl.types import InputPeerEmpty, PeerUser, PeerChat, PeerChannel
+import requests
+import time
+import random
+
+# Данные аккаунтов
+api_id1 = '12588062'
+api_hash1 = 'd0f061c565991d6ed274285c0d032f7a'
+phone1 = '+79816661471'
+api_id2 = '13123883'
+api_hash2 = 'f1fab12098570c2bf8848ad6ea23c62f'
+phone2 = '+79676939873'
+api_id3 = '19334807'
+api_hash3 = '4b4edde7a9d46172399f0ac4cb98ae63'
+phone3 = '+79842549006'
+# Создание клиентов 
+
+client1 = TelegramClient('session1', api_id1, api_hash1)
+client2 = TelegramClient('session2', api_id2, api_hash2)
+client3 = TelegramClient('session3', api_id3, api_hash3)
+
+# Авторизация
+client1.connect()
+if not client1.is_user_authorized():
+    client1.send_code_request(phone1)
+    client1.sign_in(phone1, input('1 Введите код подтверждения: '))
+client2.connect()
+if not client2.is_user_authorized():
+    client2.send_code_request(phone2)
+    client2.sign_in(phone2, input('2 Введите код подтверждения: '))
+client3.connect()
+if not client3.is_user_authorized():
+    client3.send_code_request(phone3)
+    client3.sign_in(phone3, input('3 Введите код подтверждения: '))
+# Команда для отправки нескольких сообщений с задержкой
+@client1.on(events.NewMessage(pattern='/send_multiple'))
+async def send_multiple_messages(event):
+    chat_id = event.chat_id
+# Извлечение сообщений после команды
+messages = event.raw_text.split(' ')[1:]
+for i in range(10):
+        for message in messages:
+            await client1(SendMessageRequest(chat_id, me
+ssage))
+            time.sleep(2)
+@client2.on(events.NewMessage(pattern='/send_multiple'))
+async def send_multiple_messages(event):
+    chat_id = event.chat_id
+    messages = event.raw_text.split(' ')[1:]
+    for i in range(10):
+        for message in messages:
+            await client2(SendMessageRequest(chat_id, message))
+            time.sleep(2)
+# Запуск клиентов
+client1.start()
+client2.start()
+client1.run_until_disconnected()
+client2.run_until_disconnected()
